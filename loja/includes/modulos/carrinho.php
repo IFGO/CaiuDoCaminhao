b6159ada51c74a292ff1c4f0d7374f760876d15c
wandersondsm <wdsmarques@gmail.com>
2015-07-15 21:36:16 -0300

    Modulo do carrinho

--- loja/includes/modulos/carrinho.php
+++ loja/includes/modulos/carrinho.php
@@ -1,3 +1,6 @@
+<div style="background:#EC3237;margin-top:-15px;padding: 10px;text-align: center;float:right;width:180px;">
 <a href="index.php?pagina=carrinho">
-    <img src="<?php echo $requisicoes->requestURL(); ?>/imagens/carrinho.png" width="80px" height="80px" />
+    <img style="float:left;margin-right:10px;" src="<?php echo $requisicoes->requestURL(); ?>imagens/carrinho.png" width="25px" height="25px" />
+    <h3 style="margin:0;font-size:18px;color: #fff;">Ver carrinho</h3>
 </a>
+</div>
